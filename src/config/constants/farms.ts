ffcb39951db7973f38faff5c400447a11d6178f2
daedboi <87483308+daedboi@users.noreply.github.com>
2021-09-23 21:14:12 +0300

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -88,15 +88,6 @@ const farms: FarmConfig[] = [
     quoteToken: tokens.usdc,
   },
   {
-    pid: 4,
-    lpSymbol: 'USDC-FTM LP',
-    lpAddresses: {
-      250: '0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c',
-    },
-    token: tokens.ftm,
-    quoteToken: tokens.usdc,
-  },
-  {
     pid: 21,
     isCommunity: true,
     isSpirit: true,
@@ -106,6 +97,16 @@ const farms: FarmConfig[] = [
     },
     token: tokens.papr,
     quoteToken: tokens.usdc,
+  },
+  {
+    pid: 17,
+    isCommunity: true,
+    lpSymbol: 'REAPER-FTM LP', 
+    lpAddresses: {
+      250: '0x8c24602e232b13703afcda2fb661228c9005ce8c',
+    },
+    token: tokens.reaper,
+    quoteToken: tokens.ftm,
   },
   {
     pid: 15,
