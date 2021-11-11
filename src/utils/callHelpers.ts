d813fb76a6c62e73fc3d4bef379ad2cec3815c30
daedboi <87483308+daedboi@users.noreply.github.com>
2021-11-11 15:29:26 +0200

    Set to 300k default gas limit

--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
 
-const GAS = 250000
+const GAS = 300000
 
 export const approve = async (lpContract, masterChefContract, account) => {
   return lpContract.methods
